9d5420274bfd8e7c3b800c150787d914562f720c
Julian Schneider <92728359+Julian1612@users.noreply.github.com>
2022-06-06 09:58:18 +0200

    Final Push

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: jschneid <jschneid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 12:26:57 by jschneid          #+#    #+#             */
-/*   Updated: 2022/04/13 08:44:53 by jschneid         ###   ########.fr       */
+/*   Updated: 2022/05/27 13:23:08 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	ft_putnbr_fd(int n, int fd);
 
@@ -30,4 +31,12 @@ void	ft_putnbr_fd(int n, int fd)
 	if (num > 0)
 		ft_putnbr_fd(num, fd);
 	ft_putchar_fd(mod, fd);
+}
+
+int main ()
+{
+	int i = 234;
+
+	ft_putnbr_fd(i, 1);
+	printf("%i", i);
 }
